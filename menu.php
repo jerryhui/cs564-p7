2fbefe3d1ce74a27b395339a9233cf3981906eb5
Jerry Hui <chiwei.hui@gmail.com>
2012-12-12 17:21:04 -0600

    Switch advanced_search form to php; menu change to reflect change

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <ul class="menu" role="nav">
     <li><a href="index.html">Home</a></li>
-    <li><a href="advanced_search.html">Advanced Search</a></li>
+    <li><a href="advanced_search.php">Advanced Search</a></li>
     <li><a href="browse_users.php">Browse musicians/bands</a></li>
 </ul>
