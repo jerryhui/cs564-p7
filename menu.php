c39541303c13b7d082e45ac5209ae03b0c937755
jerryhui <chiwei.hui@gmail.com>
2012-12-13 13:14:04 -0600

    add update to main menu

--- menu.php
+++ menu.php
@@ -1,5 +1,7 @@
 <ul class="menu" role="nav">
     <li><a href="index.html">Home</a></li>
+    <li><a href="update.php" title="insert/update/delete 
+users">Update</a></li>
     <li><a href="advanced_search.php">Advanced Search</a></li>
     <li><a href="browse_users.php">Browse musicians/bands</a></li>
 </ul>
